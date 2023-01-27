ca141992a35f246116fda565d6f625c944378d10
Rilana8888 <121957291+Rilana8888@users.noreply.github.com>
2023-01-27 13:57:31 +0100

    Update main.py

--- conditions/main.py
+++ conditions/main.py
@@ -4,45 +4,24 @@ __human_name__ = 'conditions'
 
 # Add your code after this line
 def farm_action (weather, time_of_day, cows_milking_status, location_of_the_cows, season, slurry_tank, grass_status):
-    if location_of_the_cows == 'pasture' and weather == 'rainy':
-        print ('take cows to cowshed') 
-    elif location_of_the_cows == 'pasture' and time_of_day == 'night':
-        print ('take cows to cowshed')
+    if location_of_the_cows == 'pasture' and weather == 'rainy' or location_of_the_cows == 'pasture' and time_of_day == 'night':
+        return 'take cows to cowshed'
+    elif cows_milking_status and location_of_the_cows == 'cowshed':
+        return 'milk cows'
     elif cows_milking_status and location_of_the_cows == 'pasture':
-        print ('take cows to cowshed')
-    elif slurry_tank and weather == 'rainy' and location_of_the_cows == 'pasture':
-        print ('take cows to cowshed')
-    elif slurry_tank and weather == 'neutral' and location_of_the_cows == 'pasture':
-        print ('take cows to cowshed')
+        return 'take cows to cowshed\nmilk cows\ntake cows back to pasture'
+    elif weather == 'rainy' and location_of_the_cows == 'cowshed' and slurry_tank or weather == 'neutral' and location_of_the_cows == 'cowshed' and slurry_tank:
+        return 'fertilze pasture'
+    elif slurry_tank and weather == 'rainy' and location_of_the_cows == 'pasture' or slurry_tank and weather == 'neutral' and location_of_the_cows == 'pasture':
+        return 'take cows to cowshed\nfertilize pasture\ntake cows back to pasture'
+    elif grass_status and season == 'spring' and weather == 'sunny' and location_of_the_cows == 'cowshed':
+        return 'mow grass'
     elif grass_status and season == 'spring' and weather == 'sunny' and location_of_the_cows == 'pasture':
-        print ('take cows to cowshed')
-    if cows_milking_status and location_of_the_cows == 'cowshed':
-        print ('milk cows')
-    elif cows_milking_status and location_of_the_cows == 'pasture':
-        print ('milk cows')
-    if slurry_tank and location_of_the_cows == 'cowshed' and weather == 'rainy':
-        print ('fertilize pasture')
-    elif slurry_tank and location_of_the_cows == 'cowshed' and weather == 'neutral':
-        print ('fertilize pasture')
-    elif slurry_tank and location_of_the_cows == 'pasture' and weather == 'rainy':
-        print ('fertilize pasture')
-    elif slurry_tank and location_of_the_cows == 'pasture' and weather == 'neutral':
-        print ('fertilize pasture')
-    if grass_status and season == 'spring' and weather == 'sunny' and cows_milking_status == False and location_of_the_cows == 'cowshed':
-        print ('mow grass')
-    elif grass_status and season == 'spring' and weather == 'sunny' and cows_milking_status == False and location_of_the_cows == 'pasture':
-        print ('mow grass')
-    if cows_milking_status and location_of_the_cows == 'pasture':
-        print ('take cows back to pasture')
-    elif slurry_tank and weather == 'rainy' and location_of_the_cows == 'pasture':
-        print ('take cows back to pasture')
-    elif slurry_tank and weather == 'neutral' and location_of_the_cows == 'pasture':
-        print ('take cows back to pasture')
-    elif grass_status and season == 'spring' and weather == 'sunny' and location_of_the_cows == 'pasture':
-        print ('take cows back to pasture')
-    else: print ('wait') 
+        return  'take cows to cowshed\nmow grass\ntake cows back to pasture'
+    else: return'wait' 
     
-farm_action('sunny', 'day', True, 'pasture', 'spring', False, True)        
+    
+print (farm_action('rainy', 'night', False, 'cowshed', 'winter', True, True))        
 
 
 
